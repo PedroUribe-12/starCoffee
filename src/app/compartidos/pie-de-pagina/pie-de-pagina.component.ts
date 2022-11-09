d295164fab79d4e23a7a34ae99ed9256fdf80b09
Jose-Ilafaya <ilafayajose@gmail.com>
2022-11-09 11:07:35 -0300

    comentarios y footer terminado

--- src/app/compartidos/pie-de-pagina/pie-de-pagina.component.ts
+++ src/app/compartidos/pie-de-pagina/pie-de-pagina.component.ts
@@ -6,9 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pie-de-pagina.component.scss']
 })
 export class PieDePaginaComponent implements OnInit {
+  //Declaracion de variables
   items:any[];
   copyright:string=''
+  //Fin de declaracion de variables
   constructor() {
+    //Inicializacion de las variables
     this.copyright='© 2022 Star Coffee. Todos los derechos reservados'
     this.items=[{
       titulo:'Inicio'
