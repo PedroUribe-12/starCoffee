d99b11b8f3097d08e3f7d4a754acc7dd046be878
iaraluna30 <iaraluna314@gmail.com>
2022-11-09 11:07:58 -0300

    Menu terminado

--- src/app/compartidos/compartidos.module.ts
+++ src/app/compartidos/compartidos.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MenuComponent } from './menu/menu.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MenuComponent
+  ],
   imports: [
     CommonModule
   ]
