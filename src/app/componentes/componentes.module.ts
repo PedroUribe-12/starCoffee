c639e183adf56f326a10c2d7cb26248641d55ece
PedroUribe-12 <orfeouribe@gmail.com>
2022-11-09 00:55:18 -0300

    portada-terminada

--- src/app/componentes/componentes.module.ts
+++ src/app/componentes/componentes.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PortadaComponent } from './portada/portada.component';
 
 
 
 @NgModule({
   declarations: [
+    PortadaComponent
   ],
   imports: [
     CommonModule
