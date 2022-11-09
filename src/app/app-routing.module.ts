36ba788860dea7d1be29fa4799a730085ad93d29
PedroUribe-12 <orfeouribe@gmail.com>
2022-11-08 22:49:57 -0300

    Subimos el proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './paginas/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'inicio',
+    pathMatch:'full'
+  },
+  {
+    path:'inicio',
+    component: InicioComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
