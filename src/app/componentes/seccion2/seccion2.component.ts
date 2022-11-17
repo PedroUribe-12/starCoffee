39f31124c4d3607280316ce9078ffeee41df5e2f
PedroUribe-12 <orfeouribe@gmail>
2022-11-17 11:45:47 -0300

    seccion2-terminada

---
+++ src/app/componentes/seccion2/seccion2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seccion2',
+  templateUrl: './seccion2.component.html',
+  styleUrls: ['./seccion2.component.scss']
+})
+export class Seccion2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
