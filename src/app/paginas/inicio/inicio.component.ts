8df1a6e83369a117a4230932e72baf9b76d73761
Tomas-Qtr <tomi30.quinteros@gmail.com>
2022-11-25 01:15:14 -0300

    Seciones terminada

--- src/app/paginas/inicio/inicio.component.ts
+++ src/app/paginas/inicio/inicio.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
+  //datos que le van a dar a un componente que esta usando input
+    titulo1:string="comidas"
+    eslogan1:string="Disfruto las mañanas con cafe"
+    imagen1:string='../../../assets/imagenes/medialunas.jpg'
+  //datos que le van a dar a un componente que esta usando input
+    titulo2:string="Cafes"
+    eslogan2:string="La mejor preparacion de cafes estan aca" //LAMEJOR PREPARACIÓN DE CAFES LA ENCONTRAS ACA
+    imagen2:string='../../../assets/imagenes/maquinas-para-cafeteria.png'
   constructor() { }
 
   ngOnInit(): void {
